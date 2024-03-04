64636a5456ac997fb0ff6be383d57f372ad5538a
bendaalimehdi <bendaalimehdi@gmail.com>
2024-03-04 18:21:31 +0100

    0403

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    "preset": "@lwc/jest-preset",
+    
 };
